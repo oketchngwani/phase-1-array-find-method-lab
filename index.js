e355963dd7c66b7dd044af49de3be6c2166affa0
Oketch <oketchngwani@gmail.com>
2025-01-19 13:13:17 +0900

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W"); // Find the first object with result "W"
+    return win ? win.year : undefined; // If found, return the year; otherwise, return undefined
+  }
+  
